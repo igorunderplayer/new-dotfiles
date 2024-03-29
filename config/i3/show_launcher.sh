c8e6fdf229afdf482c8d1fa7d54883454d73803c
igorunderplayer <igorschug6@gmail.com>
2024-03-29 16:21:13 -0300

    changes

--- config/i3/show_launcher.sh
+++ config/i3/show_launcher.sh
@@ -4,8 +4,8 @@ FILE="$HOME/.config/rofi/colors.rasi"
 
 # random accent color
 COLORS=('#8BE9DF' '#50FA7B' '#50FA7B' "#FFB86C" "#FF79C6" "#BD93F9" "#FF5555" "#F1FA8C")
-AC="${COLORS[$(( $RANDOM % 8 ))]}"
+AC="${COLORS[$(($RANDOM % 8))]}"
 sed -i -e "s/ac: .*/ac:   ${AC}FF;/g" $FILE
 sed -i -e "s/se: .*/se:   ${AC}40;/g" $FILE
 
-rofi -no-config -no-lazy-grab -show drun -modi drun -theme ~/.config/polybar/blocks/scripts/rofi/launcher.rasi
+rofi -no-config -no-lazy-grab -show drun -modi drun -theme $HOME/.config/rofi/config.rasi

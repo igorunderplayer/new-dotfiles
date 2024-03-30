1ae64d65f345f2708479ad64d852888bae1a416b
igorunderplayer <igorschug6@gmail.com>
2024-03-30 15:57:26 -0300

    move to catppuccin color theme

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,9 @@
 return {
-  -- add dracula
-  { "Mofiqul/dracula.nvim" },
-
-  -- Configure LazyVim to load dracula
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "dracula",
-      transparent = true
+      colorscheme = "catppuccin",
+      transparent = true,
     },
   },
 }
